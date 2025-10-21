e6e0e2925c4a4d0e51e12f03d72ce513eccc46de
mpupic <mpupic@uoguelph.ca>
2025-10-21 12:00:41 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,37 +1,48 @@
 #!/usr/bin/env python3
 
 # Fibonacci Sequence Exercise with functions
-# TODO: (Read detailed instructions in the Readme file)
 
 # Get the validated number of terms
 def user_input():
-  while(True):
-    print("Enter the number of Fibonacci terms you want: ")
-    terms = input()
-    if (terms.isdigit()):
-      terms = int(terms)
-      if (terms > 0):
-        break;
-      else:
-        print("Error: Please enter a positive integer greater than 0")
-    else:
-      print("Error: Please enter a positive integer greater than 0:")
-  return terms
+    while True:
+        terms_str = input("Enter the number of Fibonacci terms you want: ")
+        
+        if terms_str.isdigit():
+            terms = int(terms_str)
+            if terms > 0:
+                return terms
+            else:
+                print("Error: Please enter a positive integer greater than 0")
+        else:
+            print("Error: Invalid input. Please enter a whole number.")
 
 # Generate the sequence
-def fibonacci (terms):
-  if (terms == 0):
-    return 0
-  if (terms == 1):
-    return 1
-  else:
-    return fibonacci(terms - 1) + fibonacci (terms - 2)
+def fibonacci_sequence(terms):
+    if terms == 1:
+        return [0]
+    if terms == 2:
+        return [0, 1]
 
-def print_fib(terms):
-  for i in ranage (1, terms + 1):
-    print(fibonacci(i), end = " ")
+    sequence = [0, 1]
 
-# Print the result
-terms = user_input()
-fibonacci(terms)
-print_fib(terms)
+    for _ in range(2, terms):
+        next_term = sequence[-1] + sequence[-2]
+        sequence.append(next_term)
+
+    return sequence
+
+def print_fib(sequence):
+    print("\nFibonacci Sequence:")
+
+    output = " -> ".join(map(str, sequence))
+    print(output)
+
+def main():
+    num_terms = user_input()
+    
+    fib_list = fibonacci_sequence(num_terms)
+    
+    print_fib(fib_list)
+
+if __name__ == "__main__":
+    main()
